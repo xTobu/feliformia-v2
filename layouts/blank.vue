94b94fa63c9bf5c7c1c81bd4471ebd44972271bd
xTobu <junxiang.oriint@gmail.com>
2026-01-02 17:17:23 +0800

    [update] blank reset

--- layouts/blank.vue
+++ layouts/blank.vue
@@ -1,3 +1,24 @@
 <template>
     <slot />
 </template>
+<style lang="scss">
+body,
+html {
+    padding-right: 0px !important;
+    height: initial !important;
+    width: 100%;
+    overflow-x: hidden;
+    font-family: 'Noto Sans TC', 'Helvetica Neue', Helvetica, sans-serif;
+    text-align: center;
+    font-size: 15px;
+    min-height: 100vh;
+    position: relative;
+    margin: 0;
+}
+
+* {
+    box-sizing: border-box;
+    -webkit-touch-callout: none;
+    -webkit-tap-highlight-color: rgba(255, 255, 255, 0);
+}
+</style>
